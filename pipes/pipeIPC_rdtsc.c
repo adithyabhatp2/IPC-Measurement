a6c7432d70869a091fe5f416692faae6e2a822c0
Stephen Lee <snlee@cs.wisc.edu>
2016-02-01 09:47:08 -0600

    Implemented timing of pipe communication using rdtsc.

--- pipes/pipeIPC_rdtsc.c
+++ pipes/pipeIPC_rdtsc.c
@@ -12,98 +12,108 @@
 # include <limits.h>
 
 #include <time.h>
+#ifndef __USE_GNU
+#define __USE_GNU
+#endif
+#define _GNU_SOURCE
+#include <sched.h>
 
 // ref: http://beej.us/guide/bgipc/output/html/multipage/pipes.html
+
+static __inline__ unsigned long GetCC() {
+    unsigned a, d;
+    asm volatile("rdtsc" : "=a" (a), "=d" (d));
+    return ((unsigned long) a) | (((unsigned long) d) << 32);
+}
 
 int main(int argc, char *argv[])
 {
 
-if(argc!=2)
-	{
-	printf("Usage: %s <MSG_SIZE>\n", argv[0]);
-	exit(0);
-	}
+    // TIME STUFFF
+    unsigned long long start, end, cycles_elapsed;
+    double nanoseconds;
+    cpu_set_t cpuMask;
 
-// TIME STUFFF
-clockid_t clk_id;
-struct timespec tp_start, tp_end, res;
-int time_elapsed_sec;
-long time_elapsed_nsec;
-int retVal= 0;
-clk_id = CLOCK_MONOTONIC;
-long BILLION = 1000000000L;
-		
+    //Set to one CPU
+    CPU_ZERO(&cpuMask);
+    CPU_SET(0, &cpuMask);
+    sched_setaffinity(0, sizeof(cpuMask), &cpuMask);
 
-int MSG_SIZE = atoi(argv[1]);
-//int MSG_SIZE = (4);
-
-char recv_buf[MSG_SIZE];
-char * send_msg = (char *) calloc(MSG_SIZE+1, sizeof(char));
-
-int i;
-for(i=0;i<MSG_SIZE;i++)
-	send_msg[i] = 'A';
-	
-send_msg[MSG_SIZE] = '\0';
-//fprintf(stdout, "Msg is: %s\n", send_msg);
+    if(argc!=2)
+    {
+        printf("Usage: %s <MSG_SIZE>\n", argv[0]);
+        exit(0);
+    }
 
 
-int way1[2];
-int way2[2];
+    int MSG_SIZE = atoi(argv[1]);
+    //int MSG_SIZE = (4);
 
-/* pipe fills in the values of its arguments */
-if(pipe(way1) == -1 || pipe(way2) == -1)
-	{
-		perror("pipe failed");
-		exit(1);
-	}
-	
-if ( fork() == 0 )
-	{ 
-	/* child */
-	close(way1[1]); 
-	close(way2[0]); 
-	
-	read( way1[0], recv_buf, MSG_SIZE+1); /* read from parent */
-	//fprintf(stdout, "Child recvs : %s of size %ld\n", recv_buf, strlen(recv_buf));
-	//fprintf(stdout, "Child recvs msg of size %ld\n", strlen(recv_buf));
-	
-	write( way2[1], recv_buf, strlen(recv_buf)+1); /* write to parent */ 
-	//fprintf(stdout, "Child sent : %s\n", recv_buf);
-	//fprintf(stdout, "Child sends msg of size %ld\n", strlen(recv_buf));
-	}
-else
-	{  		
-	close(way1[0]); 
-	close(way2[1]); 
-	
-	// START TIME
-	clock_gettime(clk_id, &tp_start);
-	write( way1[1], send_msg, strlen(send_msg)+1); /* write to child */ 
-	//fprintf(stdout, "Parent sends : %s\n", send_msg);
-	//fprintf(stdout, "Parent sends msg of size %ld\n", strlen(send_msg));
-	
-	read( way2[0], recv_buf, MSG_SIZE+1); /* read from child */	
-	clock_gettime(clk_id, &tp_end);
-	// END TIME
-	
-	//fprintf(stdout, "Parent recvs : %s of size %ld\n", send_msg, strlen(send_msg));
-	fprintf(stdout, "Parent recvs msg of size %ld\n", strlen(send_msg));
-	
-	// This format should make for easy batch running..
-	// printf("%d\t%ld", MSG_SIZE, total_time);
+    char recv_buf[MSG_SIZE];
+    char * send_msg = (char *) calloc(MSG_SIZE+1, sizeof(char));
 
-	time_elapsed_sec = (tp_end.tv_sec - tp_start.tv_sec);
-	time_elapsed_nsec = (tp_end.tv_nsec - tp_start.tv_nsec);
+    int i;
+    for(i=0;i<MSG_SIZE;i++)
+        send_msg[i] = 'A';
 
-	fprintf(stdout, "Time elapsed sec: %d, nsec: %ld\n", time_elapsed_sec, time_elapsed_nsec);
-	fprintf(stdout, "Time elapsed in nanosecs : %ld\n",(BILLION*time_elapsed_sec)+time_elapsed_nsec);
-	
-	
-	}
-	
+    send_msg[MSG_SIZE] = '\0';
+    //fprintf(stdout, "Msg is: %s\n", send_msg);
+
+
+    int way1[2];
+    int way2[2];
+
+    if(pipe(way1) == -1 || pipe(way2) == -1)
+    {
+        perror("pipe failed");
+        exit(1);
+    }
+
+    if ( fork() == 0 )
+    { 
+        /* child */
+        close(way1[1]); 
+        close(way2[0]); 
+
+        read( way1[0], recv_buf, MSG_SIZE+1); /* read from parent */
+        //fprintf(stdout, "Child recvs : %s of size %ld\n", recv_buf, strlen(recv_buf));
+        //fprintf(stdout, "Child recvs msg of size %ld\n", strlen(recv_buf));
+
+        write( way2[1], recv_buf, strlen(recv_buf)+1); /* write to parent */ 
+        //fprintf(stdout, "Child sent : %s\n", recv_buf);
+        //fprintf(stdout, "Child sends msg of size %ld\n", strlen(recv_buf));
+    }
+    else
+    {  		
+        close(way1[0]); 
+        close(way2[1]); 
+
+        // START TIME
+        start = GetCC();
+        write( way1[1], send_msg, strlen(send_msg)+1); /* write to child */ 
+
+        read( way2[0], recv_buf, MSG_SIZE+1); /* read from child */	
+        end = GetCC();
+        // END TIME
+
+        //fprintf(stdout, "Parent recvs : %s of size %ld\n", send_msg, strlen(send_msg));
+        //fprintf(stdout, "Parent recvs msg of size %ld\n", strlen(send_msg));
+
+        // This format should make for easy batch running..
+
+
+        cycles_elapsed = end - start;
+        nanoseconds = ((double) cycles_elapsed) / 3.392061000;
+
+        //fprintf(stdout, "Time elapsed sec: %d, nsec: %ld\n", time_elapsed_sec, time_elapsed_nsec);
+        //fprintf(stdout, "Time elapsed in nanosecs : %lld\n",((BILLION*time_elapsed_sec)+time_elapsed_nsec)/2);
+
+        printf("%d\t%f\n", MSG_SIZE, nanoseconds/2.0);
+
+    }
 
 
 
-return 0;
+
+    return 0;
 }
